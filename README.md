74d60fda20b31bd52a276e5db13590dbf9fac09f
Julien Bisconti <julienbi@unity3d.com>
2017-09-28 13:43:12 +0300

    add readme, generate braintree creds

---
+++ README.md
@@ -0,0 +1,9 @@
+## app
+simple app for generating braintree client_token_
+## Install
+create .env file and fill your braintree creds
+merchantId=XXXXXXXXXXXX
+publicKey=XXXXXXXXXXXXX
+privateKey=XXXXXXXXXXXXXXXXXXXX
+## run
+npm run start
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import braintree from 'braintree';
 
+require('dotenv').config()
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//
-const port = process.env.PORT || 8080;
 
+const port = process.env.PORT || 8080;
 const router = express.Router();
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.get('/key', function(req, res) {
+    res.json({ message: 'key' });   
+    res.end();
+});
+
+const { merchantId, publicKey, privateKey } = process.env;
+const gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId,
+  publicKey,
+  privateKey,
+});
+
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
 });
 
 app.use('/api', router);
